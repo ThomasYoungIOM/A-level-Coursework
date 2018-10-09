77aac0ad1b09c6941ca8d9e8770f44f1bd55410e
Thomas Young <thomasyoungiom@gmail.com>
2018-10-09 15:37:39 +0100

    Worked on Desktop Program and Created Refereacnces File
    Worked on Desktop Program and Created Refereacnces File.
    
    Moved interpeate status byte to subroutine

--- Desktop Programs/Midi Serial sender/Form1.cs
+++ Desktop Programs/Midi Serial sender/Form1.cs
@@ -87,12 +87,12 @@ namespace Midi_Serial_sender
             int currentChunkLength; //The length of the current chunk
             byte statusCarry = 0;   //Last status byte that may be used for a status carry
             byte currentByte;   //Value of the byte currently being processed
+            byte eventLength;   //Used to store the length of commands that are not a set length eg SysEx messages or Meta Events
             bool error = false; //
             byte trackFormat;   //Either 0, 1 or 2
             int numOfTracks;    //Number of track chunks in the file
             int ticksPerNote;   //Speed of the thingy
-            long timeByte = 0;
-            int currentTime;
+            long deltaTime = 0;
             bool end = false;           //Has the end been reached?
 
             //Header Chunk
@@ -115,6 +115,7 @@ namespace Midi_Serial_sender
             //Read bytes until the first byte of the MThd is found
             while (midiStream.ReadByte() != 0x4D) { };
 
+            #region HeadChunk
             //Make sure that the rest of the byte match up
             if (midiStream.ReadByte() == 0x54 && midiStream.ReadByte() == 0x68 && midiStream.ReadByte() == 0x64) {
                 //Length (4 bytes) (In Head chunk should be 6, but apparently midi should be able to handle any length here)
@@ -137,10 +138,11 @@ namespace Midi_Serial_sender
                     //I'm gonna do this later
                 }
 
+                #endregion
 
 
 
-
+                #region TrackChunk
                 //Track Chunk
                 //MTtk (4 bytes)
                 if (midiStream.ReadByte() == 0x4d && midiStream.ReadByte() == 0x54 && midiStream.ReadByte() == 0x72 && midiStream.ReadByte() == 0x6b) {
@@ -151,95 +153,128 @@ namespace Midi_Serial_sender
                     do {
 
 
-                        //Time
+                        //Find the delta time byte
                         currentByte = Convert.ToByte(midiStream.ReadByte());
-
+                        deltaTime = 0;
                         while (currentByte >= 0x80) {   //While bit 8 of the byte is 1 which means the variable length is not over
-                            timeByte = (timeByte << 7) + (currentByte - 0x80);      //Bit shift the time byte left 7, then add the current byte after removing bit 8
+                            deltaTime = (deltaTime << 7) + (currentByte - 0x80);      //Bit shift the time byte left 7, then add the current byte after removing bit 8
                             currentByte = Convert.ToByte(midiStream.ReadByte());
                         }
 
-                        timeByte = (timeByte << 7) + (currentByte);                 //Bit shift the time data left 7, then add the current byte, knowing that bit 8 will be 0 cuz it's the last byte
+                        deltaTime = (deltaTime << 7) + (currentByte);                 //Bit shift the time data left 7, then add the current byte, knowing that bit 8 will be 0 cuz it's the last byte
 
 
 
                         //Status byte
                         currentByte = Convert.ToByte(midiStream.ReadByte());
-                        //
 
-                        switch ((currentByte & 0xF0) / 0x10) {        //Use the and bitwise operator to only select the left most nibble, then devide by Hex 10 to get the first nibble only
-                            case 0x8:
-                                //Note off (2 data bytes)
-                                lsvMidi.Items.Add($"Note off Ch: {currentByte & 0x0F} Note:{Convert.ToByte(midiStream.ReadByte())}  Velocity: {Convert.ToByte(midiStream.ReadByte())}");
-                                statusCarry = currentByte;
-                                break;
-
-                            case 0x9:
-                                //Note on   (2data bytes)
-                                lsvMidi.Items.Add($"Note on Ch: {currentByte & 0x0F} Note:{Convert.ToByte(midiStream.ReadByte())} Velocity: {Convert.ToByte(midiStream.ReadByte())}");
-                                statusCarry = currentByte;
-                                break;
-
-                            case 0xa:
-                                //"Polly phonic after touch" (2 data bytes)
-                                lsvMidi.Items.Add($"PollyPhonic Ch: {currentByte & 0x0F} Note:{Convert.ToByte(midiStream.ReadByte())} Velocity: {Convert.ToByte(midiStream.ReadByte())}");
-                                statusCarry = currentByte;
-                                break;
-
-                            case 0xb:
-                                //Chan mode control thingys.    ( 2data bytes)
-                                midiStream.ReadByte();
-                                midiStream.ReadByte();
-
-
-                                statusCarry = currentByte;
-                                break;
-
-                            case 0xc:
-                                //Chan Program Change           (1 data byte)
-                                lsvMidi.Items.Add($"ChanProgram change Ch: {currentByte & 0x0F} New instument: {Convert.ToByte(midiStream.ReadByte())}");
-                                statusCarry = currentByte;
-                                break;
-
-                            case 0xd:
-                                //Chanel after touch    (1 data byte)
-                                lsvMidi.Items.Add($"Channel aftertouch Ch: {currentByte & 0x0F} Data (volume, idk): {Convert.ToByte(midiStream.ReadByte())}");
-                                statusCarry = currentByte;
-                                break;
-
-                            case 0xe:
-                                //Pitch bend (2 data bytes)
-                                lsvMidi.Items.Add($"PitchBend Ch: {currentByte & 0x0F} Data1:{Convert.ToByte(midiStream.ReadByte())} Data2: {Convert.ToByte(midiStream.ReadByte())}");
-                                statusCarry = currentByte;
-                                break;
-
-                            case 0xf:
-                                //Sytem control bytes, (varying databytes)
-                                switch (currentByte & 0x0f) {
-                                    case 0x0f:
-                                        //If it's a FF byte, then that means a system is doing somthing funky and ya need to listen out for the F7 byte to signal the end of the message
-
-                                        if (midiStream.ReadByte() == 0xff) {
-                                            if(midiStream.ReadByte() == 0x2f) {
-                                                end = true;         //Tell it to end
-                                            }
-                                            
-                                        }
-                                        break;
-
-                                    default:
-                                        break;
-                                }
-                                break;
-
-                            //If no status byte, then use status carry
-                            default:
-                                lsvMidi.Items.Add("Status carry byte: " + statusCarry);
-                                break;
+                        //If the Current byte is greater than or equal to 0x80, then it is a control byte
+                        if (currentByte >= 0x80) {
+                            interpretStatusByte(currentByte, Convert.ToByte(midiStream.ReadByte()));
+                        } else {        //Else, it is a databyte and therefore status carry should be used
+                            interpretStatusByte(statusCarry, currentByte);
                         }
+
+
+
                     } while (!end);
                 }
+
+                #endregion
+
             }
+
+            void interpretStatusByte(byte controlByte, byte firstDataByte) {
+
+                switch ((controlByte & 0xF0) / 0x10) {        //Use the and bitwise operator to only select the left most nibble, then devide by Hex 10 to get the first nibble only
+                    case 0x8:
+                        //Note off (2 data bytes)
+                        lsvMidi.Items.Add($"Note off Ch: {controlByte & 0x0F} Note:{Convert.ToByte(midiStream.ReadByte())}  Velocity: {Convert.ToByte(midiStream.ReadByte())}");
+                        statusCarry = controlByte;
+                        break;
+
+                    case 0x9:
+                        //Note on   (2data bytes)
+                        lsvMidi.Items.Add($"Note on Ch: {controlByte & 0x0F} Note:{Convert.ToByte(midiStream.ReadByte())} Velocity: {Convert.ToByte(midiStream.ReadByte())}");
+                        statusCarry = controlByte;
+                        break;
+
+                    case 0xa:
+                        //"Polly phonic after touch" (2 data bytes)
+                        lsvMidi.Items.Add($"PollyPhonic Ch: {controlByte & 0x0F} Note:{Convert.ToByte(midiStream.ReadByte())} Velocity: {Convert.ToByte(midiStream.ReadByte())}");
+                        statusCarry = controlByte;
+                        break;
+
+                    case 0xb:
+                        //Chan mode control thingys.    ( 2data bytes)
+                        midiStream.ReadByte();
+                        midiStream.ReadByte();
+
+
+                        statusCarry = controlByte;
+                        break;
+
+                    case 0xc:
+                        //Chan Program Change           (1 data byte)
+                        lsvMidi.Items.Add($"ChanProgram change Ch: {controlByte & 0x0F} New instument: {Convert.ToByte(midiStream.ReadByte())}");
+                        statusCarry = controlByte;
+                        break;
+
+                    case 0xd:
+                        //Chanel after touch    (1 data byte)
+                        lsvMidi.Items.Add($"Channel aftertouch Ch: {controlByte & 0x0F} Data (volume, idk): {Convert.ToByte(midiStream.ReadByte())}");
+                        statusCarry = controlByte;
+                        break;
+
+                    case 0xe:
+                        //Pitch bend (2 data bytes)
+                        lsvMidi.Items.Add($"PitchBend Ch: {controlByte & 0x0F} Data1:{Convert.ToByte(midiStream.ReadByte())} Data2: {Convert.ToByte(midiStream.ReadByte())}");
+                        statusCarry = controlByte;
+                        break;
+
+                    case 0xf:
+                        //Sytem control bytes, (varying databytes)
+                        switch (controlByte & 0x0f) {
+                            case 0x0f:
+                                //If it's a FF byte, it's a meta event and so it'll follow the format: FF <type> <length> <bytes> 
+                                //(Found on page 136 of Referance 1, Complete midi specifation)
+
+
+                                currentByte = firstDataByte;
+                                eventLength = Convert.ToByte(midiStream.ReadByte());
+
+                                //Read the next byte that will include the type of system control byte that's being dealt with
+                                switch (currentByte) {
+                                    //End of file message
+                                    case 0x2f:
+                                        end = true;
+                                        break;
+
+                                    //Meta-event not included
+                                    default:
+                                        //If the meta event is not used, then just read to the end of it based on the length of the message
+                                        for (int i = 0; i < eventLength; i++) {
+                                            midiStream.ReadByte();
+                                        }
+                                        break;
+                                }
+
+                                break;
+
+
+
+
+                            default:
+                                break;
+                        }
+                        break;
+                }
+            }
+
+
+
+
         }
+        
     }                                                                 
 }                                                                     
